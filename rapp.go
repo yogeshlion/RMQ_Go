fd7beef2eed77a3ccc649d99c13b1b163033ce43
yogeshlion <72606996+yogeshlion@users.noreply.github.com>
2020-10-12 11:56:26 +0530

    Add files via upload

--- rapp.go
+++ rapp.go
@@ -4,70 +4,45 @@ import (
 	"fmt"
 	"log"
 	"consul"
-	//"reflect"
+	"time"
 	"github.com/streadway/amqp"
 )
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
 func consulHandle()(consul.Client1,string){
 	var addr string
 	addr= "127.0.0.1:8500"
 	_,inter2,_:=consul.NewConsulClient(addr)
-	//fmt.Println(inter1)
 	err1:=inter2.Register("app1",8000)
 	fmt.Println("Connected to Consul server....")
-	//fmt.Println(x)
 	fmt.Println(err1)
 	return inter2,"app1"
 }
 func faultHandle(inter2 consul.Client1,s string){
 	 inter2.Service(s)
 }
-
-func main() {
-	inter2,s:=consulHandle()
-	faultHandle(inter2,s)
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
+func rmqConn()(amqp.Queue,*amqp.Channel){
+	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	ch1,_ := conn.Channel()
+	q1, _ := ch1.QueueDeclare("hello",false,false,false,false,nil)
+	return q1,ch1
+	}
+func application2(q1 amqp.Queue,ch1 *amqp.Channel){
+	msgs, _ := ch1.Consume(q1.Name,"",true,false,false,false,nil)
 	forever := make(chan bool)
-
 	go func() {
 		for d := range msgs {
+			time.Sleep(500*time.Millisecond)
 			log.Printf("Received a message: %s", d.Body)
 		}
 	}()
-
 	log.Printf("Waiting for messages....")
 	<-forever
 }
+func main() {
+	inter2,s:=consulHandle()
+	q,ch:=rmqConn()
+	go faultHandle(inter2,s)
+	go application2(q,ch)
+	for{
+	}
+}
+	
--- sapp.go
+++ sapp.go
@@ -4,65 +4,40 @@ package main
 import (
 	"log"
 	"fmt"
-	//"reflect"
 	"consul"
+	"time"
 	"github.com/streadway/amqp"
 )
 func consulHandle()(consul.Client1,string){
 	var addr string
 	addr= "127.0.0.1:8500"
 	_,inter2,_:=consul.NewConsulClient(addr)
-	//fmt.Println(inter1)
 	err1:=inter2.Register("app2",5357)
 	fmt.Println("Connected to Consul server....")
-	//fmt.Println(x)
 	fmt.Println(err1)
 	return inter2,"app2"
 }
 func faultHandle(inter2 consul.Client1,s string){
 	 inter2.Service(s)
 }
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
+func rmqConn() (amqp.Queue,*amqp.Channel){
+	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	ch1,_ := conn.Channel()
+	q1, _ := ch1.QueueDeclare("hello",false,false,false,false,nil)
+	return q1,ch1
 }
-
+func application1(body string,q1 amqp.Queue,ch1 *amqp.Channel){
+	for i:=0;i<10;i++{
+		err := ch1.Publish("",q1.Name,false,false,amqp.Publishing{ContentType: "text/plain",Body: []byte(body)})
+		time.Sleep(1000*time.Millisecond)
+		fmt.Println(err)
+		log.Printf(" [x] Sent %s", body)
+}}
 func main() {
 	inter2,s:=consulHandle()
+	q,ch:= rmqConn()
 	go faultHandle(inter2,s)
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	body := "Hi"
-	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	log.Printf(" [x] Sent %s", body)
-	failOnError(err, "Failed to publish a message")
+	go application1("Data",q,ch)
 	for{
-
 	}
 }
